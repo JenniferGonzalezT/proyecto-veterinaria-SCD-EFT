e6b00b92aedb114c345935b97f10b1ec3a74afcd
JenniferGonzalezT <je.gonzalezt@duocuc.cl>
2026-05-06 03:44:41 -0400

    Modificaciones ZAP

--- cdy2203-2026-201-main/src/main/java/com/duoc/seguridadcalidad/WebSecurityConfig.java
+++ cdy2203-2026-201-main/src/main/java/com/duoc/seguridadcalidad/WebSecurityConfig.java
@@ -44,9 +44,9 @@ public class WebSecurityConfig {
                     .maxAgeInSeconds(31536000)
                 )
                 
-                // Mitiga CSP (Alerta Naranja) pero permitiendo la operatividad del frontend
+                // Mitiga CSP (Alerta Naranja) y refuerza la protección contra iframes
                 .contentSecurityPolicy(csp -> csp
-                    .policyDirectives("default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;")
+                    .policyDirectives("default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; form-action 'self'; frame-ancestors 'none';")
                 )
             )
 
